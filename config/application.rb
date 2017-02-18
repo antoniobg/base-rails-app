da6f57822f8c36a5863b5107832931630e370661
Antonio Borrero Granell <me@antoniobg.com>
2017-02-17 23:19:32 -0800

    Update DB config

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,15 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module BaseApp
+  APP_NAME = "BaseApp".freeze
+  private_constant :APP_NAME
+
+  def self.name
+    APP_NAME
+  end
+
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
