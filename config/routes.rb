d6e7569207a3c65f67aea376a40ce536bf4c379c
Antonio Borrero Granell <me@antoniobg.com>
2017-03-26 20:21:03 -0700

    Fix config

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   get "home/index"
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+
 end
