9491376dbfb39b3a970084bd101ae0fb9557eb66
Antonio Borrero Granell <me@antoniobg.com>
2017-03-26 20:21:41 -0700

    Add relationship user-identities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+  has_many :identities
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:twitter, :facebook, :github, :google_oauth2]
 
 end
